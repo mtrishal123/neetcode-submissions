9a89f8fabc78f9b0afdc0a57af3cf1e40768bf0a
Trishal Varma Mudunuri <60638143+mtrishal123@users.noreply.github.com>
2026-06-14 14:10:46 -0700

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        mapper = { ")" : "(", "]" : "[", "}" : "{" }
+        for c in s:
+            if c in mapper:
+                if stk and stk[-1] == mapper[c]:
+                    stk.pop()
+            else:
+                stk.append(c)
+        return len(stk) == 0
